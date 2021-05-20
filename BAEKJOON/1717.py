4a3bdce3636829384823eb4aff4793b0ee7629b4
Jungwoo-20 <jungwoo7250@naver.com>
2021-05-20 14:15:16 +0900

    집합의 표현

--- BAEKJOON/1717.py
+++ BAEKJOON/1717.py
@@ -1,18 +1,25 @@
 import sys
 
-sys.setrecursionlimit(10 ** 7)
+sys.setrecursionlimit(10 ** 9)
 
 
 def find(x):
     if parents[x] == x:
         return x
-    else:
-        return find(parents[x])
+    p = find(parents[x])
+    parents[x] = p
+    return p
 
 
 def union(x, y):
     if find(x) != find(y):
-        parents[find(x)] = y
+        parents[find(y)] = x
+
+
+def find_parent(x):
+    if parents[x] == x:
+        return x
+    return find_parent(parents[x])
 
 
 N, M = map(int, sys.stdin.readline().split())
@@ -20,10 +27,10 @@ parents = [i for i in range(N + 1)]
 
 for _ in range(M):
     a, b, c = map(int, sys.stdin.readline().split())
-    if a == 0:
+    if not a:
         union(b, c)
-    else:
-        if find(b) == find(c):
+    if a:
+        if find_parent(b) == find_parent(c):
             print('YES')
         else:
             print('NO')
