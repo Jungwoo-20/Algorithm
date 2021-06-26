73ae278473f0c4cf1cdc7783ccb03f6bffd50a7d
Jungwoo-20 <jungwoo7250@naver.com>
2021-06-26 22:06:44 +0900

    공유기 설치

--- BAEKJOON/2110.py
+++ BAEKJOON/2110.py
@@ -1,19 +1,22 @@
-N, C = map(int, input().split())
+import sys
+
+N, C = map(int, sys.stdin.readline().split())
 house = []
 for _ in range(N):
-    house.append(int(input()))
+    house.append(int(sys.stdin.readline()))
 house.sort()
 
 start = 1
 end = house[-1] - house[0]
+
 while start <= end:
     mid = (start + end) // 2
     cnt = 1
     setting = house[0]
-    for i in range(1, N):
-        if setting + mid <= house[i]:
+    for i in house:
+        if mid + setting <= i:
             cnt += 1
-            setting = house[i]
+            setting = i
     if cnt >= C:
         start = mid + 1
         res = mid
